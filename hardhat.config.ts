b86ac90bb588d2b5b96e7707ad356ce42756d77d
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-03-19 00:39:40 +0700

    123

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    baobap: {
+        url: "https://api.baobab.klaytn.net:8651",
+        chainId: 1001,
+        accounts: {
+            mnemonic: "cradle lounge fall undo sound ivory earn shy whisper clump craft alarm",
+        },
+    },
+},
 };
 
 export default config;
